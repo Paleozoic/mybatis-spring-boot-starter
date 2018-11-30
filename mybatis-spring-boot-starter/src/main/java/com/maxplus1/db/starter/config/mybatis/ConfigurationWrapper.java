31935a699b30095419e52de96b70092d4c6f6bb5
qiuxiaolong <qiuxiaolong@cmschina.com.cn>
2018-11-30 16:59:22 +0800

    fix mybatis conf bugs

--- mybatis-spring-boot-starter/src/main/java/com/maxplus1/db/starter/config/mybatis/ConfigurationWrapper.java
+++ mybatis-spring-boot-starter/src/main/java/com/maxplus1/db/starter/config/mybatis/ConfigurationWrapper.java
@@ -4,6 +4,6 @@ package com.maxplus1.db.starter.config.mybatis;
 import org.apache.ibatis.session.Configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("spring.maxplus1.mybatis")
+@ConfigurationProperties("spring.maxplus1.mybatis.configuration")
 public class ConfigurationWrapper extends Configuration {
 }
