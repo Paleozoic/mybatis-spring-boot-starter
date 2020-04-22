a1c857eb5cffd58ddb86ba58e7e4c146df31759e
qiuxiaolong <qiuxiaolong@cmschina.com.cn>
2020-04-22 18:21:37 +0800

    add

--- mybatis-spring-boot-starter/src/main/java/com/maxplus1/db/starter/config/common/BaseData.java
+++ mybatis-spring-boot-starter/src/main/java/com/maxplus1/db/starter/config/common/BaseData.java
@@ -1,31 +1,39 @@
 package com.maxplus1.db.starter.config.common;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BaseData<T> {
     private boolean success;
     private int code;
     private String msg;
     private T data;
 
-    public static BaseData success(Object obj){
-        return new BaseData(true, HttpStatusCode.OK.getCode(),"操作成功",obj);
+    public static BaseData success(Object obj) {
+        return new BaseData(true, HttpStatusCode.OK.getCode(), "操作成功", obj);
     }
 
-    public static BaseData failT(Object obj){
-        return new BaseData(false, HttpStatusCode.ERROR.getCode(),"操作失败",obj);
-    }
-
-
-    public static BaseData success(){
-        return new BaseData(true, HttpStatusCode.OK.getCode(),"操作成功",null);
+    public static BaseData failT(Object obj) {
+        return new BaseData(false, HttpStatusCode.ERROR.getCode(), "操作失败", obj);
     }
 
 
-    public static BaseData fail(String str){
-        return new BaseData(false, HttpStatusCode.ERROR.getCode(),str,null);
+    public static BaseData success() {
+        return new BaseData(true, HttpStatusCode.OK.getCode(), "操作成功", null);
+    }
+
+
+    public static BaseData fail(String str) {
+        return new BaseData(false, HttpStatusCode.ERROR.getCode(), str, null);
+    }
+
+    public static BaseData fail(int code, String str) {
+        return new BaseData(false, code, str, null);
     }
 }
